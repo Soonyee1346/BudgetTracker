dd9efdc62ad5ec1f6953a467f7075a6e0ce7a04b
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2026-02-02 14:04:42 +1100

    Update app.js

--- expense-tracker-api/src/app.js
+++ expense-tracker-api/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 app.use(express.json());
